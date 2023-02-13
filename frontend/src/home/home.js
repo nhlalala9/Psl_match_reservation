746e6485c3b2ccb74c84997ebcb248faa8e188e5
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2023-02-13 16:14:01 +0200

    app

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -5,56 +5,56 @@ import "./home.css";
 import Header from "./../components/header/header";
 
 function Home() {
-  const token = localStorage.getItem("token");
-  const role = localStorage.getItem("role");
-  const [user, setUser] = useState({});
+    const token = localStorage.getItem("token");
+    const role = localStorage.getItem("role");
+    const [user, setUser] = useState({});
 
-  console.log("token: ", token);
-  console.log("role: ", role);
+    console.log("token: ", token);
+    console.log("role: ", role);
 
-  return (
-    <div className="home">
-      <div className="home_container">
-        <Header />
-        <div className="background">
-          <div className="background__content">
-            <img alt="logo" className="background__content__logo" src={toy} />
-            <h1>
-              DSTV premiership
-              <br />
-              South Africa 2022/2023
-              <br />
-              <br/>
-              Tickets
-            </h1>
-            {!token ? (
-              <div className="background__content__buttons">
-                <button className="background__content__buttons__button">
-                  <a href="/login">Login</a>
-                </button>
-                <button className="background__content__buttons__button">
-                  <a href="/signup">Sign Up</a>
-                </button>
-              </div>
-            ) : (
-              //matches
+    return (
+        <div className="home">
+            <div className="home_container">
+                <Header />
+                <div className="background">
+                    <div className="background__content">
+                        <img alt="logo" className="background__content__logo" src={toy} />
+                        <h1>
+                            DSTV premiership
+                            <br />
+                            South Africa 2022/2023
+                            <br />
+                            <br />
+                            Tickets
+                        </h1>
+                        {!token ? (
+                            <div className="background__content__buttons">
+                                <button className="background__content__buttons__button">
+                                    <a href="/login">Login</a>
+                                </button>
+                                <button className="background__content__buttons__button">
+                                    <a href="/signup">Sign Up</a>
+                                </button>
+                            </div>
+                        ) : (
+                            //matches
 
-              <div className="background__content__buttons">
-                <button className="background__content__buttons__button">
-                  <a href="/matches">Matches</a>
-                </button>
-                {role === "fan" && <button className="background__content__buttons__button">
-                  <a href="/reservations">Tickets</a>
-                </button>}
-              </div>
-            )}
-          </div>
+                            <div className="background__content__buttons">
+                                <button className="background__content__buttons__button">
+                                    <a href="/matches">Matches</a>
+                                </button>
+                                {role === "fan" && <button className="background__content__buttons__button">
+                                    <a href="/reservations">Tickets</a>
+                                </button>}
+                            </div>
+                        )}
+                    </div>
 
-          <img alt="background" className="background-img" src={bg} />
+                    <img alt="background" className="background-img" src={bg} />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Home;

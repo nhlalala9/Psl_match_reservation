e25f12999a53460b4b5a2dff7a66188fac866356
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2023-03-17 08:22:19 +0200

    added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./home/home";
+import Login from "./login/login.js";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>}/>
       </Routes>
     </BrowserRouter>
 

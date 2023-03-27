dbdfbf056e6320ff63b6530e1a6b9c1ad77d7cab
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2023-03-27 14:32:11 +0200

    css

--- frontend/src/teams/teams.js
+++ frontend/src/teams/teams.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Header from '../components/header/header'
+import './teams.css';
 
 const Teams = () => {
     return (
         <div className="teams">
             <Header />
-            <div className="teams_container">
-                <div className="teams_container_header">
-                    <h1>Teams</h1>
-                </div>
+            <div className="teams_container_header">
+                <h1>Teams</h1>
             </div>
 
         </div>

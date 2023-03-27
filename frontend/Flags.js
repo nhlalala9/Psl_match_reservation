78565cce7540b155a63110bad8658f232ae1eab9
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2023-03-27 16:04:01 +0200

    imagess

---
+++ frontend/Flags.js
@@ -0,0 +1,30 @@
+// Chiefs
+import Chiefs from "./src/images/flags/chiefs.png";
+// Amazulu
+import Amazulu from "./src/images/flags/amazulu.png";
+// Sundowns 
+import Sundowns from "./src/images/flags/sundowns.png";
+// Pirates
+import Pirates from "./src/images/flags/pirates.png";
+// Chippa
+import Chippa from "./src/images/flags/chippa.png";
+// Golden Arrows
+import Arrows from "./src/images/flags/Lamontville_Golden_Arrows_logo.png";
+// Maritzburg
+import Maritzburg from "./src/images/flags/maritzburg-united.png";
+// Marumo 
+import Marumo from "./src/images/flags/Marumo_gallats_FC.png";
+// Cape Town City
+import CapeTownCity from "./src/images/flags/cape town.png";
+// Swallows
+import Swallows from "./src/images/flags/Moroka_Swallows_F.C.png";
+// Richards Bay
+import Richards from "./src/images/flags/Richards_Bay_F.C.png";
+// Sekhukhune
+import Sekhukhune from "./src/images/flags/sekhukhune.png";
+
+
+
+export default function Flags(){
+
+} 

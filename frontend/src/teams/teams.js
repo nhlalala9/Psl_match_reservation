083cce00f6258ceeee4bf95dbb2104509949f18c
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2023-03-28 12:30:24 +0200

    added

--- frontend/src/teams/teams.js
+++ frontend/src/teams/teams.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
 import Header from '../components/header/header'
 import './teams.css';
 
+
 const Teams = () => {
+ 
     return (
         <div className="teams">
             <Header />
             <div className="teams_container_header">
                 <h1>Teams</h1>
             </div>
+            <div className="teams__content">
+                <div className="teams_list">
 
+                        <div className="teams_list__item" >
+                            <div className="country">
+                               
+                            </div>
+                        </div>
+                    
+                </div>
+            </div>
         </div>
     )
 }
